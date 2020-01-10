d2faac5372cdbedd0efccba658a34af92568cc73
litvinenkomaxim <litvinenkomaxim@gmail.com>
2020-01-10 18:28:51 +0200

    5

--- part2/script1.js
+++ part2/script1.js
@@ -5,28 +5,15 @@
 
 
 function createTimer() {
-  let t0;
-  t0 = performance.now()
 
-  function sum(a, b) {
-    a = a + 10;
-    b = b * 10;
-    let c = a + b;
-  }
+  t0 = performance.now()
+  alert("!");
   t1 = performance.now();
   console.log('На выполнение функции');
-  console.log(sum);
   console.log('потрачено ' + (t1 - t0) + ' мкс');
   console.log(t1 - t0);
   return (t1 - t0);
-  sum(2, 5);
-  reurn(sum)
 }
-//createTimer();
-let timer = createTimer();
-//console.log(timer() + '    22222');
-//timer();
-//console.log(timer() + '    22222');
-
-console.log(sum); // код, время выполнения которого нужно измерить
-console.log(timer); // время в мкс от начала выполнения createTimer() до момента вызова timer()
+createTimer();
+// let timer = createTimer();
+// timer()
