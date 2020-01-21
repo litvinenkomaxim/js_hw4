fefc3a0c9b0f937b471cc884fdf23a495f608042
litvinenkomaxim <litvinenkomaxim@gmail.com>
2020-01-21 18:28:24 +0200

    21.01_4

--- hw6_functional/js.js
+++ hw6_functional/js.js
@@ -1,3 +1,8 @@
+function Canvas(id) {
+  this.canvas = document.getElementById(id)
+  this.ctx = this.canvas.getContext('2d')
+}
+
 class Canvas {
   constructor(id) {
     this.canvas = document.getElementById(id)
@@ -37,7 +42,7 @@ class Line extends Figure {
   }
 }
 
-class Line2 extends Figure {
+/*class Line2 extends Figure {
   constructor(x, y, x2, y2, color) {
     super(x, y, color)
     this.x2 = x2
@@ -74,7 +79,7 @@ class Line2 extends Figure {
 }
 
 
-class Rect extends Figure {
+/*class Rect extends Figure {
   constructor(x, y, w, h, color) {
     super(x, y, color)
     this.w = w
@@ -88,7 +93,7 @@ class Rect extends Figure {
 
   }
 }
-class Circle extends Figure {
+/*class Circle extends Figure {
   constructor(x, y, rad, color) {
     super(x, y, color)
     this.rad = rad
@@ -105,16 +110,15 @@ class Circle extends Figure {
     ctx.closePath()
   }
 }
-
+*/
 
 const line = new Line(50, 250, 200, 200, 'red')
-const line2 = new Line(60, 260, 210, 210, 'green')
-const rect = new Rect(260, 130, 60, 120, 'blue')
-const rect2 = new Rect(290, 100, 130, 40, 'black')
-const rect3 = new Rect(400, 130, 50, 100, 'yellow')
-const circle = new Circle(120, 120, 50, 'green')
-const circle2 = new Circle(150, 150, 30, 'red')
-
-const line7 = new Line2(10, 10, 10, 10, 'red')
+// const line2 = new Line(60, 260, 210, 210, 'green')
+// const rect = new Rect(260, 130, 60, 120, 'blue')
+// const rect2 = new Rect(290, 100, 130, 40, 'black')
+// const rect3 = new Rect(400, 130, 50, 100, 'yellow')
+// const circle = new Circle(120, 120, 50, 'green')
+// const circle2 = new Circle(150, 150, 30, 'red')
+// const line7 = new Line2(10, 10, 10, 10, 'red')
 const drawArea = new Canvas('c1')
 drawArea.add(line, line2, rect, rect2, rect3, circle, line7, circle2)
